389675a9eef093d83112e8a8b1a42568235fe212
darbs <darby.perez@gmail.com>
2018-06-04 22:23:33 -0500

    config wrapper

---
+++ config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/tkanos/gonfig"
+)
+
+type Configuration struct {
+	DbName     string
+	DbEndpoint string
+	MqEndpoint string
+}
+
+var config Configuration
+
+func init() {
+	env := os.Getenv("MODE")
+	configPath := os.Getenv("CONFIG_PATH")
+
+	filename := ""
+	switch {
+	case env == "production":
+		filename = "prod.json"
+	case env == "development":
+		filename = "dev.json"
+	case env == "test":
+		filename = "test.json"
+	}
+
+	if filename != "" {
+		log.Printf("Loading configs - %v", filename)
+	}
+
+	if configPath == "" {
+		configPath = "configs"
+	}
+
+	fp := filepath.Join(configPath,filename)
+	err := gonfig.GetConf(fp, &config)
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+		panic(err)
+	}
+}
+
+func GetConfig() Configuration {
+	return config
+}
